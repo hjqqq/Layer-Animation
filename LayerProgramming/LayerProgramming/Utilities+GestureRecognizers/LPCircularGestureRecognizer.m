74807af617ee86deacebbae0a2bc253d79b70e5b
Mahesh Shanbhag <mahesh.shanbhag@ymedialabs.com>
2012-08-30 18:28:24 +0530

    Adding chsnges to the for detecting the circular rotation

--- LayerProgramming/LayerProgramming/Utilities+GestureRecognizers/LPCircularGestureRecognizer.m
+++ LayerProgramming/LayerProgramming/Utilities+GestureRecognizers/LPCircularGestureRecognizer.m
@@ -23,6 +23,8 @@
 
 - (void)resetData;
 - (void)setHolePortion:(CGFloat)holePortion;
+- (BOOL)validatePosition:(CGPoint)touchPoint;
+
 @end
 
 @implementation LPCircularGestureRecognizer
@@ -52,6 +54,14 @@
     _maximumRadius = MIN([self view].frame.size.width, [self view].frame.size.height)/2;
     _minimumRadius = _holePortion * _maximumRadius;
     
+    // once the radius is obtained get the center of the circle
+    _circleCenter = CGPointMake(CGRectGetMidX([self view].frame), CGRectGetMidY([self view].frame));
+    
+    if(![self validatePosition:_startPoint])
+    {
+        // loggging error that the point is in side the doughnut
+    }
+    
 }
 
 - (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event
@@ -65,6 +75,26 @@
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
+    // get the touch object
+    UITouch *_touch = [touches anyObject];
+    
+    // get the previous and new point
+    CGPoint _previousPoint = [_touch previousLocationInView:[self view]];
+    CGPoint _newPoint = [_touch locationInView:[self view]];
+    
+    // here to ge the velocity of the movement, we need the position of the 
+    // points with center as the reference.
+    
+    CGPoint _translateOld = CGPointMake(_previousPoint.x - _circleCenter.x, _previousPoint.y -_circleCenter.y);
+    CGPoint _translateNew = CGPointMake(_newPoint.x - _circleCenter.x, _newPoint.y - _circleCenter.y);
+    
+    // we need to get the angle made by the swipe so that to calculate the 
+    // velocity
+    CGFloat _newTangentPoint = atan2f(_translateOld.x, _translateOld.y);
+    CGFloat _oldTangentPoint = atan2f(_translateNew.x, _translateNew.y);
+    
+    
+    
     
 }
 
@@ -103,4 +133,21 @@
         [NSException exceptionWithName:@"LPCircularGestureRecognizer" reason:@"The hole portion is out value (Unit Scale) " userInfo:nil];
     }
 }
+
+- (BOOL)validatePosition:(CGPoint)touchPoint
+{
+  // get the x and y of the touch point from the center of the circle
+    CGFloat _touchX = touchPoint.x - _circleCenter.x;
+    CGFloat _touchY = touchPoint.y - _circleCenter.y;
+    
+    CGFloat _distance = sqrtf(powf(_touchX, 2)+powf(_touchY, 2));
+    
+    if(_distance > _maximumRadius && _distance < _minimumRadius)
+    {
+        return true;
+    }
+    
+    return false;
+}
+
 @end
